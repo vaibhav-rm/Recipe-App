1dc6b533ffdaecb1e6d24140ff297b413e38964f
vaibhav-rm <rathodvaibhav401@gmail.com>
2023-12-22 20:21:21 +0530

    Auth system

--- home/urls.py
+++ home/urls.py
@@ -5,5 +5,8 @@ from home import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('delete_recipe/<id>/', views.delete_recipe, name="delete_recipe"),
-    path('update_recipe/<id>/', views.update_recipe, name="update_recipe")
+    path('update_recipe/<id>/', views.update_recipe, name="update_recipe"),
+    path('login/', views.login_page, name="login"),
+    path('logout/', views.logout_page, name="logout"),
+    path('register/', views.register_page, name="register")
 ]
--- home/views.py
+++ home/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render, redirect
 from .models import *
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 # Create your views here.
 
+@login_required(login_url="/login/")
 def index(request):
     if request.method == "POST":
          
@@ -24,11 +29,13 @@ def index(request):
     context = {'recipes': queryset}
     return render(request, 'home/index.html', context)
 
+@login_required(login_url="/login/")
 def delete_recipe(request, id):
     queryset = Recipe.objects.get(id = id)
     queryset.delete()
     return redirect("/")
 
+@login_required(login_url="/login/")
 def update_recipe(request, id):
     queryset = Recipe.objects.get(id = id)
 
@@ -49,3 +56,56 @@ def update_recipe(request, id):
         return redirect('/')
     context = {'recipe': queryset}
     return render(request, 'home/update_recipes.html', context)
+
+def login_page(request):
+
+    if request.method =="POST":
+        
+        data = request.POST
+        username = data.get('username')
+        password = data.get('password')
+
+        if not User.objects.filter(username = username).exists():
+            messages.error(request, 'Invalid Username')
+            return redirect('/login/')
+        
+        user = authenticate(request, username = username, password = password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('/')
+        else:
+            messages.warning(request, "Invalid Credentials")
+            return redirect('/login/')
+
+    return render(request, "home/login.html")
+
+def logout_page(request):
+    logout(request)
+    return redirect('/login/')
+
+def register_page(request):
+    
+    if request.method == "POST":
+        
+        data = request.POST
+        first_name = data.get('first_name')
+        last_name = data.get('last_name')
+        username = data.get('username')
+        password = data.get('password')
+        
+        if User.objects.filter(username = username).exists():
+            messages.warning(request, "Username already taken")
+            return redirect('/register/')
+
+        user = User.objects.create_user(
+                first_name = first_name,
+                last_name = last_name,
+                username = username
+                )
+        
+        user.set_password(password)
+        user.save()
+        messages.success(request, "Account Created Scucessfully")
+
+    return render(request, "home/register.html")
